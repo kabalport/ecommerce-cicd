8f15e3404614c1cb91be14feef14c933e580fba7
kabalport <kabalport@gmail.com>
2024-04-20 21:06:51 +0900

    Initial commit

---
+++ src/main/java/com/example/ecommercecicd/EcommerceCicdApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommercecicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceCicdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceCicdApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ecommercecicd/EcommerceCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommercecicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcommerceCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
