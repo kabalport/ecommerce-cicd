4b993a3233f9dfbef27375036a74e44778d0e13e
kabalport <kabalport@gmail.com>
2024-04-28 22:08:52 +0900

    productRead mock test

--- src/main/java/com/example/ecommercecicd/busienss/ProductException.java
+++
@@ -1,7 +0,0 @@
-package com.example.ecommercecicd.busienss;
-
-public class ProductException extends RuntimeException {
-    public ProductException(String message) {
-        super(message);
-    }
-}
---
+++ src/main/java/com/example/ecommercecicd/member/application/UserPointService.java
@@ -0,0 +1,35 @@
+package com.example.ecommercecicd.member.application;
+
+import com.example.ecommercecicd.member.business.domain.Member;
+import com.example.ecommercecicd.member.business.exception.UserPointBadRequestException;
+import com.example.ecommercecicd.member.business.exception.UserPointError;
+import com.example.ecommercecicd.member.business.exception.UserPointException;
+import com.example.ecommercecicd.member.business.repository.MemberRepository;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+import static com.example.ecommercecicd.member.business.exception.UserPointError.USER_NOT_FOUND;
+
+@Service
+public class UserPointService {
+    private final MemberRepository memberRepository;
+    private final UserPointValidate validator = new UserPointValidate();
+
+    public UserPointService(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
+
+    public void charge(String userId, BigDecimal chargePoint) {
+        validator.validate(chargePoint);
+        Member member = memberRepository.findByUserId(userId).orElseThrow(() -> new UserPointBadRequestException(USER_NOT_FOUND));
+        member.chargeUserPoint(chargePoint);
+    }
+
+
+    public void use(String userId, BigDecimal usePoint) {
+        validator.validate(usePoint);
+        Member member = memberRepository.findByUserId(userId).orElseThrow(() -> new UserPointBadRequestException(USER_NOT_FOUND));
+        member.useUserPoint(usePoint);
+    }
+}
